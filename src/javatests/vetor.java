50f53f7b2dbf588ca7fa83b391be8ab132e43f82
Abysmum <abysmum666@gmail.com>
2015-08-19 23:51:59 -0300

    Adding methods.

---
+++ src/javatests/vetor.java
@@ -0,0 +1,33 @@
+//Related to student.java
+package javatests;
+
+import java.util.Arrays;
+
+public class vetor {
+	private Student[] students = new Student[100];
+	
+	public void add(Student student){
+		//implement
+	}
+	
+	public void add(int pos, Student student){
+		//implement 
+	}
+	
+	public Student gets(int pos){
+		//implement
+	}
+	
+	public void remove(int pos){
+		//implement
+	}
+	
+	public int size(){
+		//implement
+	}
+	
+	public String toString(){
+		return Arrays.toString(students);
+	}
+
+}
