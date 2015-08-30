1136c48ac1eab4bdc4dcb8df2a4ff9ef711bcf9c
Abysmum <abysmum666@gmail.com>
2015-08-30 12:56:20 -0300

    Update initial structure.

---
+++ src/piles/Piece.java
@@ -0,0 +1,14 @@
+package piles;
+
+public class Piece {
+	//era essa peça que você queria???
+	private String name;
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+}
---
+++ src/piles/Pile.java
@@ -0,0 +1,17 @@
+package piles;
+
+public class Pile {
+	public void insert(Piece piece){
+		
+	}
+	
+	public Piece remove(){
+		return null;
+	}
+	
+	public boolean empty(){
+		return false;
+	}
+	
+	
+}
