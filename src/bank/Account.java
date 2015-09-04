f594d03bd21afcc5e27a514a7b5a5458c03d858f
Abysmum <abysmum666@gmail.com>
2015-09-03 23:15:42 -0300

    Bank OO.

---
+++ src/bank/Account.java
@@ -0,0 +1,19 @@
+package bank;
+
+public class Account {
+	private String name;
+	
+	private int balance;
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public int getBalance(){
+		return this.balance;
+	}
+}
